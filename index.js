8dbc45ba291b3b51035ff39e4f256971d97a23a0
AleksandraRataj <aleksandrarataj2@gmail.com>
2020-11-07 19:05:26 +0100

    Edit index.js

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 const express = require('express');
+
+const port = 8000;
+const env = process.env.NODE_ENV || 'development';
+const app = express();
+
+app.get('/', (request, response) => {
+    response.send({msg: "Test"})
+});
+
+app.listen(port, '127.0.0.1', () => {
+    console.log(`Server is listening on http://127.0.0.1:${port} in ${env} mode`)
+});
