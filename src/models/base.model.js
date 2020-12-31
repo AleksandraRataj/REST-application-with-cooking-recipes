d02b9a66dbbab51fa32a05e4eb9c799ce6980f16
Aleksandra Rataj <aleksandrarataj2@gmail.com>
2020-12-31 17:21:31 +0100

    Update models scripts

--- src/models/base.model.js
+++ src/models/base.model.js
@@ -1,5 +1,5 @@
-const {Model} = require('objection')
-const knex = require('../knex')
+const {Model} = require('objection');
+const knex = require('../knex');
 
 Model.knex(knex);
 
@@ -10,10 +10,12 @@ class BaseModel extends Model {
         this.updated_at = new Date().toISOString();
     }
 
-    $beforeUpdate(queryContext) {
+    $beforeUpdate(opt, queryContext) {
         this.updated_at = new Date().toISOString();
     }
 
+    //jesli użytkownik przesle dane które zmodyfikowałyby id,
+    // to nie będzie miał możliwości zmodyfikowania id, tak żeby nie nadpisał go w BD
     $formatDatabaseJson(json) {
         json = super.$formatDatabaseJson(json);
         delete json.id;
--- src/models/recipe.model.js
+++ src/models/recipe.model.js
@@ -1,15 +1,20 @@
 const {Model} = require('objection');
+//korzystanie z połączenia za pomocą biblio knex która jest już skonfigurawana
+// wczytujemy skonfigurowaną instancję obiektu knex
 const knex = require('../knex');
 const BaseModel = require("./base.model");
 
+//konfigurujemy model, zainicjaliwany w ten sposób, że można go połączyć z bazą danych
 Model.knex(knex);
 
+//model - obiektowa interpretacja tabeli w bazie danych
 class Recipe extends BaseModel {
 
     static get tableName(){
         return 'recipes';
     }
 
+    //walidacja danych przed zapisaniem ich do bazy
     static get jsonSchema(){
         return {
             type: 'object',
